b2269fd5d3c6ae11ff0a4427b5940e795e7ca796
mehike <siin.mina@mail.ee>
2019-08-09 20:37:51 +0300

    Project custom exception handling

--- ppmtool/src/main/java/eu/babywatchers/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/eu/babywatchers/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package eu.babywatchers.ppmtool.services;
 
 import eu.babywatchers.ppmtool.domain.Project;
+import eu.babywatchers.ppmtool.exceptions.ProjectIdException;
 import eu.babywatchers.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateproject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists!");
+        }
     }
 
 }
